d3f98c0ad97e6342d5c9c6599b7f89ad0a3a47c5
alex <amartinezgonzalez.cf@iesesteveterradas.cat>
2023-12-15 16:52:20 +0100

    .

---
+++ src/main/java/cat/iesesteveterradas/Main.java
@@ -0,0 +1,39 @@
+package cat.iesesteveterradas;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        final int windowWidth = 800;
+        final int windowHeight = 600;
+
+        UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
+        UtilsViews.addView(getClass(), "layout_principal", "/assets/layout_principal.fxml");
+        UtilsViews.addView(getClass(), "layout_encriptar", "/assets/layout_encriptar.fxml");
+        UtilsViews.addView(getClass(), "layout_desencriptar", "/assets/layout_desencriptar.fxml");
+
+        Scene scene = new Scene(UtilsViews.parentContainer);
+        
+        stage.setScene(scene);
+        stage.setTitle("Animació entre vistes");
+        stage.setMinWidth(windowWidth);
+        stage.setMinHeight(windowHeight);
+        stage.show();
+
+        // Add icon only if not Mac
+        if (!System.getProperty("os.name").contains("Mac")) {
+            Image icon = new Image("file:/icons/icon.png");
+            stage.getIcons().add(icon);
+        }
+    }
+}
